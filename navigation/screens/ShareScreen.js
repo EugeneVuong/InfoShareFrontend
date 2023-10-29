3372c0b19601392c719341e70fa0612c3645ee22
Aywei2001 <aywei2013@gmail.com>
2023-10-29 15:33:47 -0700

    qr format update

--- navigation/screens/ShareScreen.js
+++ navigation/screens/ShareScreen.js
@@ -1,15 +1,39 @@
 import * as React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, Image, StyleSheet} from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 export default function ShareScreen({navigation}) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center '}}>
-            <Text 
-                style={{
-                    fontSize: 26,
-                    fontWeight: 'bold'
-                }}>
-                Share Screen
-            </Text>
+        <View style={{ alignItems: 'center', justifyContent: 'center '}}>
+            <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+                <Text 
+                    style={{
+                        fontSize: 36,
+                        fontWeight: 'bold',
+                        lineHeight: 72,
+                    }}>
+                    To Share Your Info:
+                </Text>
+                <Text 
+                    style={{
+                        fontSize: 26,
+                        fontWeight: 'bold',
+                        lineHeight: 92,
+                    }}>
+                    Scan Code Below
+                </Text>
+                    
+                <QRCode
+                    value="React-Native"
+                    size={270}
+                    stye={{
+                        lineHeight: 72,
+                    }}
+                />
+            </View>
         </View>
     );
+
+    const styles = StyleSheet.create({
+
+    });
 }
