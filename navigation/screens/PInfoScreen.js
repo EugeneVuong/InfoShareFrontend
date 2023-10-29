f73426ae866ffccb645a33659ff53a1528b62c42
Aywei2001 <aywei2013@gmail.com>
2023-10-28 20:56:50 -0700

    edit commit

--- navigation/screens/PInfoScreen.js
+++ navigation/screens/PInfoScreen.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, TextInput, StyleSheet} from 'react-native';
 export default function PInfoScreen({navigation}) {
+    const [text, onChangeText] = React.useState('Useless Text');
+    const [number, onChangeNumber] = React.useState('');
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center '}}>
             <Text 
@@ -10,6 +13,24 @@ export default function PInfoScreen({navigation}) {
                 }}>
                 Personal Information Screen
             </Text>
+            <TextInput
+                style = {styles.input}
+                onChangeText = {onChangeText}
+                value = {text}
+                placeholder='First Name'
+            />
+            <Text>
+
+            </Text>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+})
