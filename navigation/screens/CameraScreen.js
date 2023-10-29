b8b2815b82435041aa59d6f29ad3a9436d16f129
Eugene Vuong <vuongeugene@gmail.com>
2023-10-29 10:10:31 -0700

    QR Code Scanner

--- navigation/screens/CameraScreen.js
+++ navigation/screens/CameraScreen.js
@@ -1,12 +1,58 @@
-import * as React from 'react';
-import { View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import { useIsFocused } from '@react-navigation/native';
 export default function CameraScreen({navigation}) {
+    const [hasCameraPermission, setHasCameraPermission] = useState(null);
+    const [camera, setCamera] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back)
+    const [scanned, setScanned] = useState(false);
+    const [result, setResult] = useState('Nothing scanned yet');
+    
+    const scanHandler = ({type, data}) => {
+        setScanned(false)
+        setResult(data)
+        console.log(data)
+        setTimeout(() => {
+            setScanned(true)
+        }, 3000)
+    }
+    
+    useEffect(() => {
+        (async () => {
+          const cameraStatus = await Camera.requestCameraPermissionsAsync();
+          setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+      }, []);
+    
+    if (hasCameraPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
     
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center '}}>
-            <Text>
-                Test
-            </Text>
+        <View style={{ flex: 1}}>
+            <View style={styles.cameraContainer}>
+                    { useIsFocused() && <Camera 
+                    ref={ref => setCamera(ref)}
+                    style={styles.fixedRatio} 
+                    type={type}
+                    ratio={'1:1'}
+                    onBarCodeScanned={scanned && scanHandler}
+                    /> }
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    cameraContainer: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    fixedRatio:{
+        flex: 1,
+        aspectRatio: 1
+    }
+})
+  
