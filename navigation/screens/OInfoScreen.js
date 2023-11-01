16dde76b2fb156bb76bda6838129f8e6c951bf06
Erick Chan <ericks3500@gmail.com>
2023-11-01 16:53:01 -0700

    Added Style to OInfoScreen

--- navigation/screens/OInfoScreen.js
+++ navigation/screens/OInfoScreen.js
@@ -2,19 +2,32 @@ import React from 'react';
 import {View, Text, StyleSheet, ScrollView} from 'react-native';
 export default function OInfoScreen(props) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-evenly'}}>
+            <Text style={styles.text}>
                 First Name: {props.firstName}
             </Text>
-            <Text>
+            <Text style={styles.text}>
                 Last Name: {props.lastName}
             </Text>
-            <Text>
+            <Text style={styles.text}>
                 Phone Number: {props.phoneNumber}
             </Text>
-            <Text>
+            <Text style={styles.text}>
                 Major: {props.major}
             </Text>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    text: {
+        'fontSize': 25,
+        'padding': 10,
+        'backgroundColor': 'skyblue',
+        'borderColor': 'black',
+        'overflow': 'hidden',
+        'borderWidth': 5,
+        'borderRadius': 10,
+    
+    }
+});
