888aa6c276b668e419d46582feabc6689ebefcc5
ovelazqz <105611315+ovelazqz@users.noreply.github.com>
2023-11-01 16:54:26 -0700

    added clerk

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import MainContainer from './navigation/MainContainer'
-
+import { ClerkProvider } from "@clerk/clerk-react";
+ 
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+ 
 
 function App(){
   return(
+    <ClerkProvider publishableKey={clerkPubKey}>
     <MainContainer/>
+    </ClerkProvider>
   );
 }
 
